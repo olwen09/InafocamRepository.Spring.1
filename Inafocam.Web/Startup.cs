bb59540c47c907c89827abbf989264fc3d8dda1e
olwen henriquez diaz <olwen.henriquez@utreee.com>
2020-08-18 17:27:23 -0400

    Cambios

--- Inafocam.Web/Startup.cs
+++ Inafocam.Web/Startup.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Reflection;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Andamios.Web.Areas.Clientes.Models;
 using Andamios.Web.Helpers;
 using Andamios.Web.Models;
 using Inafocam.core.Modelos;
@@ -67,20 +66,10 @@ namespace Andamios.Web
             {
                 op.AddPolicy(PolicyName.AdminUser, policy => policy.RequireRole(RoleName.AdministradorInafocam));
                 op.AddPolicy(PolicyName.UsuarioEjecutivo, policy => policy.RequireRole(RoleName.UsuarioEjecutivoInafocam));
-                //op.AddPolicy(PolicyName.UsuarioEjecutivo, policy => policy.RequireRole(RoleName.UsuarioEjecutivoUniversitario));
+
                 op.AddPolicy(PolicyName.GestionUniversitaria, policy => policy.RequireRole(RoleName.GestionUniversitaria));
             });
-            //services.AddScoped<IInventario, InventarioRepository>();
 
-
-
-
-            //#if DEBUG
-            //            //windows
-            //            string filePath = $@"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\libwkhtmltox.dll";
-
-            //#endif
-            //            context.LoadUnmanagedLibrary(filePath);
 
             services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
 
@@ -111,10 +100,7 @@ namespace Andamios.Web
             //////
             services.ConfigureApplicationCookie(options =>
             {
-                // Cookie settings
-                //options.Cookie.HttpOnly = true;
 
-                //options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
 
                 options.LoginPath = "/Usuarios/Usuario/Login";
                 options.AccessDeniedPath = "/identity/Account/AccessDenied";
@@ -131,144 +117,17 @@ namespace Andamios.Web
                 options.Password.RequiredLength = 6;
                 options.Password.RequiredUniqueChars = 1;
 
-                // Lockout settings.
-                //options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-                //options.Lockout.MaxFailedAccessAttempts = 5;
-                //options.Lockout.AllowedForNewUsers = true;
 
-                // User settings.
                 options.User.AllowedUserNameCharacters =
                     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
                 options.User.RequireUniqueEmail = false;
             });
 
-            //services.ConfigureApplicationCookie (options => {
-            //    // Cookie settings
-            //    options.Cookie.HttpOnly = true;
-            //    options.ExpireTimeSpan = TimeSpan.FromMinutes (5);
-
-            //==== vickry
-            //// Maneno de cookies y login path
-            // services.ConfigureApplicationCookie (options => {
-            //     // Cookie settings
-            //     options.Cookie.HttpOnly = true;
-            // ===== end
-            //     options.ExpireTimeSpan = TimeSpan.FromMinutes (60);
-
-            //    options.LoginPath = "/Identity/Account/Login";
-            //    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-            //    options.SlidingExpiration = true;
-            //});
-
-            //var context = services.BuildServiceProvider().GetService<AndamiosDominicanosContext>();
-
-            // manejador de cookies para retornar al login
-            //services.ConfigureApplicationCookie (option => {
-            //    option.LoginPath = "/Usuarios/Usuario/Login";
-            //});
-
-            //services.AddAuthorization(options =>
-            //{
-            //    foreach (var modulo in context.Modulos)
-            //    {
-            //        // Generaci�n de permisos
-            //        for (int i = 0; i < permisos.Length; i++)
-            //        {
-            //            var politica = modulo.Codigo + "." + permisos[i];
-            //            // PERMISOS_A_MODULOS
-
-            //            switch (permisos[i].ToLower())
-            //            {
-            //                case "read":
-            //                    options.AddPolicy(politica,
-            //                        policy =>
-            //                        policy
-            //                        .RequireAssertion(context =>
-            //                           context.User.HasClaim(
-            //                               c =>
-            //                               c.Value == modulo.Codigo + ".Read" ||
-            //                               c.Value == modulo.Codigo + ".Create" ||
-            //                               c.Value == modulo.Codigo + ".Update" ||
-            //                               c.Value == modulo.Codigo + ".Admin"
-            //                           ))
-            //                        .RequireClaim(Constante.Module, modulo.Codigo));
-            //                    break;
-            //                case "create":
-            //                    options.AddPolicy(politica,
-            //                        policy =>
-            //                        policy
-            //                        .RequireAssertion(context =>
-            //                           context.User.HasClaim(
-            //                               c =>
-            //                               c.Value == modulo.Codigo + ".Create" ||
-            //                               c.Value == modulo.Codigo + ".Update" ||
-            //                               c.Value == modulo.Codigo + ".Admin"
-            //                           ))
-            //                        .RequireClaim(Constante.Module, modulo.Codigo));
-            //                    break;
-            //                case "update":
-            //                    options.AddPolicy(politica,
-            //                        policy =>
-            //                        policy
-            //                        .RequireAssertion(context =>
-            //                           context.User.HasClaim(
-            //                               c =>
-            //                               c.Value == modulo.Codigo + ".Update" ||
-            //                               c.Value == modulo.Codigo + ".Admin"
-            //                           ))
-            //                        .RequireClaim(Constante.Module, modulo.Codigo));
-            //                    break;
-            //                case "admin":
-            //                    options.AddPolicy(politica,
-            //                        policy =>
-            //                        policy
-            //                        .RequireAssertion(context =>
-            //                           context.User.HasClaim(
-            //                               c =>
-            //                               c.Value == modulo.Codigo + ".Admin"
-            //                           ))
-            //                        .RequireClaim(Constante.Module, modulo.Codigo));
-            //                    break;
-            //            }
-            //        }
-
-            //        // MODULOS
-            //        options.AddPolicy(modulo.Codigo, p => p.RequireClaim(Constante.Module, modulo.Codigo));
-            //    }
-
-            //    //options.AddPolicy("Create", policy => policy.Requirements.Add(new HasScopeRequirement("denegado", domain)));
-            //});
 
 
-
-            //services.AddLocalization(options => { options.ResourcesPath = "Resources"; });
 
             services.AddMvc();
-           //.AddViewLocalization(options =>
-           //{
-           //    options.ResourcesPath = "Resources";
-           //})
-           //.AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
-           //.AddDataAnnotationsLocalization();
-
-            //services.Configure<RequestLocalizationOptions>(options =>
-            //{
-            //    var supportedCultures = new[]
-            //    {
-            //            new CultureInfo(esCulture),
-            //            new CultureInfo("en")
-            //        };
-
-            //    options.DefaultRequestCulture = new RequestCulture(culture: esCulture, uiCulture: esCulture);
-            //    options.SupportedCultures = supportedCultures;
-            //    options.SupportedUICultures = supportedCultures;
-
-            //    options.AddInitialRequestCultureProvider(new CustomRequestCultureProvider(async context =>
-            //    {
-            //        // My custom request culture logic
-            //        return new ProviderCultureResult("es");
-            //    }));
-            //});
+ 
 
             // conexiones de para las inyecciones de dependencias
 
@@ -336,7 +195,6 @@ namespace Andamios.Web
 
             #endregion
 
-            //services.AddTransient<IValidator<ClienteModel>, ClienteModel.ClienteModelValidator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -353,23 +211,6 @@ namespace Andamios.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            //var supportedCultures = new[]
-            // {
-            //        new CultureInfo(esCulture),
-            //        new CultureInfo("en"),
-            //    };
-
-            //app.UseRequestLocalization(new RequestLocalizationOptions
-            //{
-            //    DefaultRequestCulture = new RequestCulture(esCulture),
-            //    // Formatting numbers, dates, etc.
-            //    SupportedCultures = supportedCultures,
-            //    // UI strings that we have localized.
-            //    SupportedUICultures = supportedCultures
-            //});
-
-
 
 
             app.UseHttpsRedirection();
@@ -393,16 +234,7 @@ namespace Andamios.Web
                 endpoints.MapRazorPages();
             });
 
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapControllerRoute(
-            //        name: "Areas",
-            //        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
-            //    endpoints.MapControllerRoute(
-            //        name: "default",
-            //        pattern: "{controller=Home}/{action=Index}/{id?}");
-            //});
         }
     }
 }
